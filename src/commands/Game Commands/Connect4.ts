ce1d866ce3418d93823f62e91c70ef607e5152fa
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-08-18 19:18:30 +1200

    Added ties and horizontal and vertical wins

--- src/commands/Game Commands/Connect4.ts
+++ src/commands/Game Commands/Connect4.ts
@@ -1,3 +1,4 @@
+import { MessageReaction } from "discord.js";
 import { RunFunction } from "../../interfaces/Command";
 
 enum State {
@@ -15,7 +16,7 @@ class Board {
         [ State.Empty, State.Empty, State.Empty, State.Empty, State.Empty, State.Empty, State.Empty ],
     ];
 
-    public Add(index: number, piece: State): boolean {
+    public Add(index: number, piece: State): [success: boolean, player_won: boolean] {
         let y = -1;
         for (let i = 0; i < 5; i++) {
             if (this.state[i][index] === State.Empty) {
@@ -27,9 +28,76 @@ class Board {
 
         if (y != -1) {
             this.state[y][index] = piece;
-            return true;
+
+            let won = false;
+
+            // Left
+            if (index >= 3) {
+                let row = true;
+                for (let i = 0; i < 3; i++) {
+                    if (this.state[y][index - i - 1] !== piece) {
+                        row = false;
+                        break;
+                    }
+                }
+
+                if (row) {
+                    won = row;
+                }
+            }
+
+            // Right
+            if (index <= 3) {
+                let row = true;
+
+                for (let i = 0; i < 3; i++) {
+                    if (this.state[y][index + i + 1] !== piece) {
+                        row = false;
+                        break;
+                    }
+                }
+
+                if (row) {
+                    won = row;
+                }
+            }
+
+            // Up
+            if (y >= 3) {
+                let row = true;
+
+                for (let i = 0; i < 3; i++) {
+                    if (this.state[y - i - 1][index] !== piece) {
+                        row = false;
+                        break;
+                    }
+                }
+
+                if (row) {
+                    won = row;
+                }
+            }
+
+            // Down
+            if (y <= 1) {
+                let row = true;
+
+                for (let i = 0; i < 3; i++) {
+                    if (this.state[y + i + 1][index] !== piece) {
+                        row = false;
+                        break;
+                    }
+                }
+
+                if (row) {
+                    won = row;
+                }
+            }
+
+            return [true, won];
         }
-        return false;
+
+        return [false, false];
     }
 
     public Draw(turn: State): string {
@@ -78,6 +146,20 @@ class Board {
 
         return text;
     }
+
+    public Full(): boolean {
+        let foundSpace = false;
+
+        for (let y = 0; y < 5; y++) {
+            for (let x = 0; x < 7; x++) {
+                if (this.state[y][x] == State.Empty) {
+                    foundSpace = true;
+                }
+            }
+        }
+
+        return !foundSpace;
+    }
 }
 
 export const run: RunFunction = async(client, message, args) => {
@@ -94,17 +176,25 @@ export const run: RunFunction = async(client, message, args) => {
     const board = new Board();
 
     const msg = await message.channel.send(client.embed({ description: board.Draw(currentPiece) }, message));
-    await msg.react('1️⃣');
-    await msg.react('2️⃣');
-    await msg.react('3️⃣');
-    await msg.react('4️⃣');
-    await msg.react('5️⃣');
-    await msg.react('6️⃣');
-    await msg.react('7️⃣');
+    let reactions: MessageReaction[] = [];
+    reactions.push(await msg.react('1️⃣'));
+    reactions.push(await msg.react('2️⃣'));
+    reactions.push(await msg.react('3️⃣'));
+    reactions.push(await msg.react('4️⃣'));
+    reactions.push(await msg.react('5️⃣'));
+    reactions.push(await msg.react('6️⃣'));
+    reactions.push(await msg.react('7️⃣'));
+
+    const Shutdown = () => {
+        reactions.forEach(reaction => {
+            reaction.remove();
+        });
+        collector.stop();
+    };
 
     const filter = (reaction, user) => ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣'].includes(reaction.emoji.name);
     let collector = msg.createReactionCollector(filter, { time: 2147483647 });
-    collector.on('collect', (reaction, collector) => {
+    collector.on('collect', (reaction, _) => {
         if ((reaction.users.cache.last() == player1 && currentPiece === State.Yellow) || (reaction.users.cache.last() == player2 && currentPiece === State.Red)) {
             let index: number;
             switch (reaction.emoji.name) {
@@ -137,8 +227,23 @@ export const run: RunFunction = async(client, message, args) => {
                 } break;
             }
 
-            if (board.Add(index, currentPiece)) {
+            let [success, won] = board.Add(index, currentPiece);
+            if (success) {
                 currentPiece = currentPiece === State.Yellow ? State.Red : State.Yellow;
+            }
+
+            if (won) {
+                const text = board.Draw(currentPiece) + '\n' + `'${currentPiece == State.Yellow ? player2.username : player1.username}' Won!`;
+                msg.edit(client.embed({ description: text }, message));
+                Shutdown();
+                return;
+            }
+
+            if (board.Full()) {
+                const text = board.Draw(currentPiece) + '\n' + `'${player1.username}' and '${player2.username}' Tied!`;
+                msg.edit(client.embed({ description: text }, message));
+                Shutdown();
+                return;
             }
 
             msg.edit(client.embed({ description: board.Draw(currentPiece) }, message));
